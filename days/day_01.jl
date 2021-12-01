217c9b8d2a6c31333ceee633d3c74e30620b2672
Andrew Hynes <andrewjhynes@gmail.com>
2021-12-01 15:05:12 -0500

    feat: add solution to day 1

---
+++ days/day_01.jl
@@ -0,0 +1,15 @@
+using Printf
+
+using RollingFunctions
+
+
+path_data = "data/01.txt"
+
+depths = parse.(Int, readlines(path_data))
+answer_1 = count(diff(depths) .> 0)
+
+sums_rolling = rolling(sum, depths, 3)
+answer_2 = count(diff(sums_rolling) .> 0)
+
+println("Number of increases: ", answer_1)
+println("Number of rolling sum increases: ", answer_2)
